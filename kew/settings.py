06dd2cf9784ca43d1d13acab4171fac3c3730865
keshav <keshavjha1504@gmail.com>
2021-07-12 21:24:44 +0530

    allowed hosts chnaged

--- kew/settings.py
+++ kew/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["keshaveworld.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
